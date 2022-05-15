657eb8b5919673bc3a14897dd00677c08930d2d7
ST4NSB <barbulescuadrian10@gmail.com>
2022-05-15 23:04:30 +0300

    some small changes

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 settings = dict(
-    number_of_games = 20,
+    number_of_games = 100,
     load_model=True,
     color = 'black', # black or white
     bot_level = 1, # 1 or 2,
     show_debug=False,
     save_games=False,
-    checkpoint_location='checkpoints\\model_weights.h5',
+    checkpoint_location='checkpoints\\4conv_20_1000.h5',
     dataset_location='datasets\\othello_dataset.csv',
     xml_location='datasets\\othello_xml.xml',
     my_games_location='datasets\\othello_self_games.csv'
@@ -14,7 +14,7 @@ settings = dict(
 model = dict(
     model_type='4conv',
     batch_size = 32, # 32
-    epochs = 20, # 20
+    epochs = 100, # 20
     max_loaded_matches = 1500, # 1500, 1000
     split_validation = 0.8
 )
